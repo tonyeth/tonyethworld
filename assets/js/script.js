ce6c5ae882bb9844460fd1575449544e218cc468
Antonio Quistelli <antonio.quistelli@gmail.com>
2024-05-22 08:48:47 +0200

    last

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,48 +3,54 @@ document.addEventListener('DOMContentLoaded', function() {
     const menu = document.querySelector('.menu-hidden');
 
     hamburger.addEventListener('click', function() {
-        // Alterna la visibilità del menu
         menu.classList.toggle('visibile');
     });
 
-    // Chiude il menu cliccando al di fuori
     document.addEventListener('click', function(event) {
         if (!menu.contains(event.target) && !hamburger.contains(event.target)) {
             menu.classList.remove('visibile');
         }
     });
 
-    // Previene la chiusura cliccando all'interno
+    
     menu.addEventListener('click', function(event) {
         event.stopPropagation();
     });
 });
+
 function SendMail() {
-
    
+    var from_name = document.getElementById("from_name").value.trim();
+    var email = document.getElementById("email").value.trim();
+    var profession = document.getElementById("profession").value.trim();
+    var industry = document.getElementById("industry").value.trim();
+    var message = document.getElementById("message").value.trim();
 
-    var params = {
-        name: document.getElementById("from_name").value,
-        email: document.getElementById("email").value,
-        profession: document.getElementById("profession").value,
-        industry: document.getElementById("industry").value,
-        message: document.getElementById("message").value
-    };
     
     if (!from_name || !email || !profession || !industry || !message) {
-        alert("Please fill in all the required fields before submitting.");
+        alert("All fields must be filled out!");
         return; 
     }
 
+   
+    var params = {
+        from_name: from_name,
+        email: email,
+        profession: profession,
+        industry: industry,
+        message: message
+    };
+
+   
     emailjs.send("service_dvn88zd", "template_lkolvrw", params)
     .then(function(response) {
         alert("Success! Email sent, status: " + response.status);
-        document.getElementById("contact-form").reset();
+        document.getElementById("contact-form").reset(); 
     }, function(error) {
         alert("Failed to send email: " + error.text);
     });
 }
-(function(){
-    emailjs.init("ScyjE1eYaPsCYSzW2");
-})();
 
+(function(){
+    emailjs.init("ScyjE1eYaPsCYSzW2"); 
+})();
